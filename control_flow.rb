afee0576304575af849cb63e420de049443a7843
douglassmoss <douglassmoss@icloud.com>
2022-01-29 19:38:34 -0800

    commit tree ad38a755ed4b14434c37bffb7ba9fe3aab61a4b6

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,43 @@
 def admin_login(username, password)
-  # your code here
+  if username = "admin" || "ADMIN" && password = "12345"
+    return "Access granted"
+  else 
+    return "Access denied"
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  case temperature
+  when temperature<40
+    puts "It's brisk out there!"
+  when temperature>=40 && temperature<= 65
+    puts "It's a little chilly out there!"
+  when temperature>50
+    puts "It's too dang hot out there!"
+  else
+    puts "It's perfect out there!"
 end
 
 def fizzbuzz(num)
-  # your code here
+  if (num % 3 === 0)
+    puts "Fizz"
+  elsif (num % 5===0)
+    puts "Buzz"
+  elsif (num % 3===0 && num % 5===0)
+    puts "FizzBuzz"
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "-"
+    return num1 - num2
+  when "+"
+    return num1 + num2
+  when "*"
+    return num1*num2
+  when "/"
+    return num1/num2
+  else
+    puts "Invalid Input"
+    return nil
 end
 
